8cb6d5652edccf060546d703a0db7964fc4b50a6
Sneha Sunny <snehasunny321@gmail.com>
2022-10-17 10:11:38 +0530

    Lastest

--- celerytask/celerytask/settings.py
+++ celerytask/celerytask/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pa4ysgk4w+3emmero8!$cq65z6k1$hkok3#nqizhrkrus14x3b'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
